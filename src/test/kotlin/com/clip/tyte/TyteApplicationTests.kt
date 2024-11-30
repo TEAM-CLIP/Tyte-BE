378909db3aecf72b8450e3b8e8ba73872e23f440
isprogrammingfun <wisjang1@naver.com>
2024-11-30 21:40:14 +0900

    Initial commit

---
+++ src/test/kotlin/com/clip/tyte/TyteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clip.tyte
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TyteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
