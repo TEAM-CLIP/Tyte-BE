49f20b9456a252f91fa2b13a4cfbc1b5a2d78aba
isprogrammingfun <wisjang1@naver.com>
2025-01-13 18:10:39 +0900

    feat : 사용자 회원가입/탈퇴, 로그아웃 관련 usecase 및 구현체 생성

---
+++ Application-Module/src/main/kotlin/com/clip/application/user/port/in/LogoutUsecase.kt
@@ -0,0 +1,10 @@
+package com.clip.application.user.port.`in`
+
+interface LogoutUsecase {
+    fun logout(command: Command)
+
+    data class Command(
+        val refreshToken: String,
+        val userId: String,
+    )
+}
