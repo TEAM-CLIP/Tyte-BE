3374d3e5b789453085f875e483cd2c15fdd06d7d
isprogrammingfun <wisjang1@naver.com>
2025-01-14 20:53:11 +0900

    feat : 태그 도메인 엔티티 생성

---
+++ Domain-Module/src/main/kotlin/com/clip/domain/tag/entity/Tag.kt
@@ -0,0 +1,35 @@
+package com.clip.domain.tag.entity
+
+import com.clip.domain.common.DomainId
+
+data class Tag(
+    val id: DomainId = DomainId.generate(),
+    val userId: String,
+    val name: String,
+    val color: String,
+)  {
+    companion object {
+        fun createDefaultTags(userId: DomainId): List<Tag> {
+            return listOf(
+                Tag(
+                    id = DomainId.generate(),
+                    userId = userId.value,
+                    name = "학습",
+                    color = "FF0000"
+                ),
+                Tag(
+                    id = DomainId.generate(),
+                    userId = userId.value,
+                    name = "여가",
+                    color = "F0E68C"
+                ),
+                Tag(
+                    id = DomainId.generate(),
+                    userId = userId.value,
+                    name = "건강",
+                    color = "00FFFF"
+                )
+            )
+        }
+    }
+}
