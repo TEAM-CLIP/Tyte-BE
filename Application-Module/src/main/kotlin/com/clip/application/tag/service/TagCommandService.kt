c51cbd53614a85bd8b3d0da4694f53b67ce9fafe
isprogrammingfun <wisjang1@naver.com>
2025-01-17 01:13:36 +0900

    feat : 태그 생성, 조회, 수정 usecase 구현

--- Application-Module/src/main/kotlin/com/clip/application/tag/service/TagCommandService.kt
+++ Application-Module/src/main/kotlin/com/clip/application/tag/service/TagCommandService.kt
@@ -1,17 +1,39 @@
 package com.clip.application.tag.service
 
-import com.clip.application.tag.port.`in`.CreateTagUsecase
-import com.clip.application.tag.port.`in`.UpdateTagUsecase
+import com.clip.application.tag.port.`in`.usecase.CreateTagUsecase
+import com.clip.application.tag.port.`in`.usecase.DeleteTagUsecase
+import com.clip.application.tag.port.`in`.usecase.UpdateTagUsecase
+import com.clip.application.tag.port.out.TagManagementPort
+import com.clip.domain.common.DomainId
+import com.clip.domain.tag.entity.Tag
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class TagCommandService : CreateTagUsecase, UpdateTagUsecase{
+@Transactional
+class TagCommandService(
+    private val tagManagementPort: TagManagementPort
+) : CreateTagUsecase, UpdateTagUsecase, DeleteTagUsecase {
     override fun create(command: CreateTagUsecase.Command) {
-        TODO("Not yet implemented")
+        val tag = Tag(
+            userId = DomainId(command.userId),
+            name = command.name,
+            color = command.color
+        )
+        tagManagementPort.saveTag(tag)
     }
 
     override fun update(command: UpdateTagUsecase.Command) {
-        TODO("Not yet implemented")
+        val tag = tagManagementPort.getTagNotNull(
+            tagId = DomainId(command.tagId),
+            userId = DomainId(command.userId)
+        )
+        tag.update(
+            name = command.name,
+            color = command.color
+        )
+
+        tagManagementPort.saveTag(tag)
     }
 
     override fun delete(command: DeleteTagUsecase.Command) {
